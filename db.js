a52b09b833022a921647307d731b50b457583996
Pattp2017 <pafptp@gmail.com>
2026-09-16 11:14:38 -0300

    Update db.js

--- db.js
+++ db.js
@@ -6,10 +6,10 @@
 (function () {
 
   const SUPABASE_URL =
-    'https://dbleblnwolbbxtscjxif.supabase.co';
+    'https://uofnninqxnsvaemxpwhh.supabase.co';
 
   const SUPABASE_KEY =
-    'sb_publishable_RIq2RdCrwvjvZc7CswobVg_0BlBfRSd';
+    'sb_publishable_kCX0kt1EFYYL0sR3zQHDzg_6rLVRJQ8';
 
   const TABELA_VISTORIAS =
     'checklist_vistorias';
